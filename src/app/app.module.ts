83c6f6b730d0309f9e54dff58b148b3015c25b82
Trevor Ross <trevor@tjrcadd.com>
2020-04-12 16:53:22 -0400

    added header footer and sitedetail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,17 @@ import { SiteserviceService} from './services/siteservice.service';
 import { AppComponent } from './app.component';
 import { SitelistComponent } from './components/sitelist/sitelist.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SitedetailComponent } from './components/sitedetail/sitedetail.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SitelistComponent
+    SitelistComponent,
+    SitedetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/sitedetail/sitedetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sitedetail',
+  templateUrl: './sitedetail.component.html',
+  styleUrls: ['./sitedetail.component.scss']
+})
+export class SitedetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
